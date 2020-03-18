b6957048c2668ee9070b8977e439d76eef184f51
shaharnu11 <shaharnu11@gmail.com>
2020-03-18 13:34:13 +0200

    simple registration and email confirmation. (#17)
    * Bump django from 2.2.9 to 2.2.10
    
    Bumps [django](https://github.com/django/django) from 2.2.9 to 2.2.10.
    - [Release notes](https://github.com/django/django/releases)
    - [Commits](https://github.com/django/django/compare/2.2.9...2.2.10)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Temp
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Pini Houri <pinhasph@gmail.com>

--- src/auth/auth_models/user.py
+++ src/auth/auth_models/user.py
@@ -4,13 +4,14 @@ from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext_lazy as _
 import uuid
 
+
 class BurnerUserManager(UserManager):
     def create_superuser(self, email, password, **extra_fields):
         print(email, password)
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('birthday', '1971-01-01')
-        
+
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
@@ -19,31 +20,33 @@ class BurnerUserManager(UserManager):
         return self._create_user(email, email, password, **extra_fields)
 
 
-
 class BurnerUser(AbstractUser):
-
     REQUIRED_FIELDS = []
     USERNAME_FIELD = 'email'
-
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    email = models.EmailField(_('email address'), unique=True, db_index=True, blank=True)
-    passport_country = models.CharField(max_length=15, default='IL')
-    country_code = models.CharField(max_length=15, default='IL')
-    city = models.TextField()
-    street = models.TextField()
     address = models.TextField()
-    birthday = models.DateField()
-    primary_phone_number = PhoneNumberField(default='', blank=True)
+    birthday = models.DateField(null=True)
+    city = models.TextField()
+    country_code = models.CharField(max_length=15, default='IL')
+    email = models.EmailField(_('email address'), unique=True, db_index=True, blank=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     identification_number = models.CharField(max_length=15, null=False, blank=False)
-    identification_type = models.CharField(default='tz', max_length=15, blank=False, null=False)
+    identification_type = models.CharField(choices=(('tz', 'tz'), ('ps', 'ps'),), default='tz', max_length=2)
+    passport_country = models.CharField(max_length=15, default='IL')
+    primary_phone_number = PhoneNumberField(default='', blank=True)
+
+    # state - verified,
 
     objects = BurnerUserManager()
 
     class Meta:
-
         unique_together = ('identification_number', 'identification_type')
-
 
     def clean(self):
         self.username = self.email
         super().clean()
+
+    def save(self, *args, **kwargs):
+        super().save(args, kwargs)
+
+    def get_model_fields(self):
+        return self._meta.fields
