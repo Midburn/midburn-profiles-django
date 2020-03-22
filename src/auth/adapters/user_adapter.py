576ed4dab42550e78ed42d878243bdbe5a9417ec
Pini Houri <pinhasph@gmail.com>
2020-03-22 14:00:15 +0200

    Feat | Sendgrid | Integrate with sendgrid templates (#19)
    * Use sendgrid
    
    * Update dockerfile, use sendgrid

--- src/auth/adapters/user_adapter.py
+++ src/auth/adapters/user_adapter.py
@@ -4,9 +4,5 @@ from allauth.account.adapter import DefaultAccountAdapter
 class CustomAccountAdapter(DefaultAccountAdapter):
 
     def send_mail(self, template_prefix, email, context):
-        a = 5
         print(template_prefix, email)
-
-    def send_confirmation_mail(self, request, email_confirmation, signup):
-        a = 5
-        print(email_confirmation.key)
+        pass
